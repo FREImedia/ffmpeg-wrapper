f6ef6120f48d84708ef1eec1995f4413487d8752
Matthias Frei <mf@frei.media>
2016-08-03 16:37:22 +0200

    Updated buildscript

--- ffmpeg_build_script/build_for_android.sh
+++ ffmpeg_build_script/build_for_android.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
-NDK=~/Android/android-ndk-r10e
-SYSROOT=$NDK/platforms/android-16/arch-arm/
-TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64
 
+###
+# This script builds FFMPEG and setups the project to work "out-of-the-box"
+# Simply modifiy the paths in the SETUP-part of this file.
+# The script will build ffmpeg and place the files in the android project
+# (do not modify project structure!)
+###
+
+
+# SETUP
+NDK=~/Library/Android/sdk/ndk-bundle
+FFMPEG_SRC=/Users/mat/Downloads/ffmpeg-2.7.7
+
+FLAVOUR=armeabi-v7a
 CPU=arm
-PREFIX=android/$CPU
+
+
+# DO NOT CHANGE BELOW THIS LINE
+# ==============================
+# ( Okay, maybe you can change this if you know what you are doing :-) )
+#
+SYSROOT=${NDK}/platforms/android-16/arch-arm/
+TOOLCHAIN=${NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+
+PREFIX=android/${CPU}
 ADDI_CFLAGS="-marm -g"
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export PATH=$TOOLCHAIN/bin:$PATH
-export CC=arm-linux-androideabi-gcc
-export LD=arm-linux-androideabi-ld
-export AR=arm-linux-androideabi-ar
+export PATH=${TOOLCHAIN}/bin:$PATH
+export CC=${TOOLCHAIN}/bin/arm-linux-androideabi-gcc-4.9
+export LD=${TOOLCHAIN}/bin/arm-linux-androideabi-ld
+export AR=${TOOLCHAIN}/bin/arm-linux-androideabi-ar
 
 
-function build_android
-{
-	./configure 												\
-		--prefix=$PREFIX 										\
+# Build ffmpeg for android
+function build_android {
+    echo "Building FFMPEG..."
+	./configure 									\
+		--prefix=${PREFIX} 										\
 		--enable-shared 										\
 		--disable-symver										\
 		--disable-static										\
@@ -24,12 +45,12 @@ function build_android
 		--disable-ffplay										\
 		--disable-ffprobe										\
 		--enable-cross-compile 									\
-		--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- 	\
+		--cross-prefix=${TOOLCHAIN}/bin/arm-linux-androideabi- 	\
 		--target-os=linux 										\
 		--arch=arm 												\
-		--sysroot=$SYSROOT 										\
-		--extra-cflags="-O0 -fpic $ADDI_CFLAGS" 				\
-		--extra-ldflags="$ADDI_LDFLAGS" 						\
+		--sysroot=${SYSROOT}									\
+		--extra-cflags="-O0 -fpic ${ADDI_CFLAGS}" 				\
+		--extra-ldflags="${ADDI_LDFLAGS}" 						\
 		--disable-doc 											\
 		--disable-htmlpages  									\
 		--disable-manpages 										\
@@ -37,14 +58,84 @@ function build_android
 		--disable-txtpages 										
 
 
-	rm /dist
-	make -j7
+	rm -rf ${FFMPEG_SRC}/${PREFIX}
+	make
 	make install
 }
-echo $NDK
-# build_android
 
-$CC -lm -lz -shared --sysroot=$SYSROOT -Wl,--no-undefined -Wl,-z,noexecstack $EXTRA_LDFLAGS libavutil/*.o		\
-libavutil/arm/*.o libavcodec/*.o libavcodec/arm/*.o libavformat/*.o libswresample/*.o libswscale/*.o -o			\
-./libffmpeg-all.so
 
+# workaround for duplicate functions (dirty)
+function remove_duplicates {
+    echo "Removing duplicates (Workaround..)"
+
+    rm ${FFMPEG_SRC}/libavutil/log2_tab.o
+    rm ${FFMPEG_SRC}/libavcodec/golomb.o
+    rm ${FFMPEG_SRC}/libavformat/log2_tab.o
+    rm ${FFMPEG_SRC}/libswscale/log2_tab.o
+    rm ${FFMPEG_SRC}/libswresample/log2_tab.o
+    rm ${FFMPEG_SRC}/libavfilter/log2_tab.o
+}
+
+
+
+# combine .so files to a single one
+function combine_libs {
+    echo "Combining libraries to a single one..."
+
+    $CC -lm -lz -shared --sysroot=${SYSROOT} -Wl,--no-undefined -Wl,-z,noexecstack ${EXTRA_LDFLAGS} \
+    libavutil/*.o libavutil/arm/*.o libavcodec/*.o libavcodec/arm/*.o libavformat/*.o libavfilter/*.o   \
+    libavdevice/*.o libswresample/*.o libswscale/*.o libswresample/arm/*.o compat/*.o   \
+     -o ./libffmpeg-all.so
+}
+
+
+# setting up project
+function setup_project {
+    echo "Setting up project and copying files..."
+    cd ${ABSOLUTE_PATH}
+
+    # Libs
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniLibs/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniLibs/${FLAVOUR}
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniLibs/${FLAVOUR}/bin
+    cp ${FFMPEG_SRC}/${PREFIX}/bin/* ${ABSOLUTE_PATH}/../app/src/main/jniLibs/${FLAVOUR}/bin/
+    cp ${FFMPEG_SRC}/libffmpeg-all.so ${ABSOLUTE_PATH}/../app/src/main/jniLibs/${FLAVOUR}/libffmpeg.so
+    cp ${FFMPEG_SRC}/${PREFIX}/libs/* ${ABSOLUTE_PATH}/../app/src/main/jniLibs/${FLAVOUR}/
+
+
+    # Header
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}
+    rm -rf ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/
+    rm -rf ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/
+    cp -R ${FFMPEG_SRC}/${PREFIX}/include/* ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/
+
+    # Other headers
+    cp ${FFMPEG_SRC}/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/compat/
+    cp ${FFMPEG_SRC}/compat/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/compat/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavresample/
+    cp ${FFMPEG_SRC}/libavresample/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavresample/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavutil/
+    cp ${FFMPEG_SRC}/libavutil/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavutil/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavformat/
+    cp ${FFMPEG_SRC}/libavformat/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libavformat/
+    mkdir -p ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libpostproc/
+    cp ${FFMPEG_SRC}/libpostproc/*.h ${ABSOLUTE_PATH}/../app/src/main/jniInclude/${FLAVOUR}/libpostproc/
+
+}
+
+
+# RUN
+echo "NDK path: ${NDK}"
+echo "FFMPEG path: ${FFMPEG_SRC} \n"
+cd ${FFMPEG_SRC}
+
+build_android
+remove_duplicates
+combine_libs
+setup_project
+
+
+echo "Done"
